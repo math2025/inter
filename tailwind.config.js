5b5017cd351bf2128002a6d477656601aa82d18c
balu <tattavijayabalaji@gmail.com>
2025-06-24 01:39:07 +0530

    fixed all

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        "fade-in": "fadeIn 0.5s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0, transform: "translateY(20px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
